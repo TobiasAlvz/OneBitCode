5236d8ebf1985b8511bfdf6dac37ee43d16c8b0f
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2022-12-12 10:56:56 -0300

    Js
    Js

---
+++ JavaScript-Moderno/spreadOperator/script.js
@@ -0,0 +1,6 @@
+const students = ["João", "Igor", "Marcos", "Geraldo"];
+
+const newStudents = students;
+
+console.log(newStudents);
+console.log(students);
---
+++ JavaScript-Moderno/templateLiterals/script.js
@@ -0,0 +1,5 @@
+const name = "Tobias";
+
+console.log(`Olá ${name}!
+Hoje é ${Date()}
+    2+2 = ${2 + 2}`);
