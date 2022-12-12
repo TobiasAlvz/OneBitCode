3632f085c92f00f3e2198b6dacc95eb0363d483e
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2022-12-12 11:24:49 -0300

    Update script.js

--- JavaScript-Moderno/spreadOperator/script.js
+++ JavaScript-Moderno/spreadOperator/script.js
@@ -1,6 +1,14 @@
 const students = ["João", "Igor", "Marcos", "Geraldo"];
+// Não é modificado
+const moreStudents = [...students];
 
 const newStudents = students;
 
+// Modifica os dois
+newStudents.pop();
+newStudents.push("Maria");
+moreStudents.push("Maria");
+
 console.log(newStudents);
 console.log(students);
+console.log(...moreStudents);
