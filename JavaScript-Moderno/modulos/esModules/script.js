2170f311ff28015e2d17d72b9acc5f6009cee842
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2022-12-23 11:25:55 -0300

    g

---
+++ JavaScript-Moderno/modulos/esModules/script.js
@@ -0,0 +1,13 @@
+import { name, input, label, br } from "./functions.js";
+
+console.log(label({ for: "fullname", textContent: "Nome Completo" }));
+
+console.log(
+  input({
+    id: "fullname",
+    name: "fullname",
+    placeholder: "Digite seu nome completo...",
+  })
+);
+console.log(br());
+console.log(name);
